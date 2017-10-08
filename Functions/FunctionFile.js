f3a3fb1f498e5e49816636147e042931384b1148
DESKTOP-T2AK48F\dell <shyam.sundar525@gmail.com>
2017-10-08 16:11:39 +0545

    Update files

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -1,8 +1,90 @@
-﻿var item;
+﻿
 Office.initialize = function () {
-    item = Office.context.mailbox.item;
 }
 
+function trackUrlAndLink(event) {
+    var item = Office.context.mailbox.item;   
+
+    var body;  
+
+    item.body.getAsync(Office.CoercionType.Html, { asyncContext: "This is passed to the callback" }, function callback(asyncResult) {
+        if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+            body = asyncResult.value;
+            Office.context.mailbox.item.to.getAsync(function callback(result)
+            {
+                if (result.status == Office.AsyncResultStatus.Succeeded) {
+                    var arrayOfToRecipients = result.value;
+                    replaceBodyText(body, arrayOfToRecipients, event);
+                }
+                else {
+                    writeLog('error in recipients')
+                }
+            });
+        }
+        else {
+            writeLog('error in body')
+        }
+    });  
+  
+}
+
+function replaceBodyText(body,listRecipient,event)
+{
+    var emailList = "?id_op=";
+    var isRecipients = false;
+    if (listRecipient != null)
+    {
+        var count = listRecipient.length;
+        for (var i = 0; i < count; i++)
+        {
+            var item = listRecipient[i];
+            if (count - 1 == i)
+            {
+                emailList += item.emailAddress;
+            } else {
+                emailList += item.emailAddress+"&";
+            }
+           
+            isRecipients = true;
+        }
+
+    }
+    if (isRecipients && body != null)
+    {
+      
+        var href_regex = /<a.*?href=.*?>/g;
+       
+        var matches = body.match(href_regex);
+        //Iterate loop to find href and relace it
+        for (var i = 0; i < matches.length; i++)
+        {
+            //var singleMatch = matches[i];
+            var url = matches[i];
+            if (url != null)
+            {
+                var updateUrl = url.replace("\">", emailList + "\">");
+                body = body.replace(url, updateUrl);
+            }
+        }
+        //update body
+        Office.context.mailbox.item.body.setAsync(
+            body,
+            { coercionType: "html", asyncContext: "This is passed to the callback" },
+            function callback(resultUp) {
+                if (resultUp.status == Office.AsyncResultStatus.Succeeded) {
+                    statusUpdate("icon16", "Tracking infor is added");
+                    event.completed();
+                }
+            });
+    }
+    
+}
+
+
+function writeLog(message)
+{
+
+}
 // Helper function to add a status message to the info bar.
 function statusUpdate(icon, text) {
   Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
@@ -15,98 +97,4 @@ function statusUpdate(icon, text) {
 
 function defaultStatus(event) {
   statusUpdate("icon16" , "Hello World!");
-}
-
-function doSomethingAndShowDialog(event) {
-            clickEvent = event;
-            //writeToDoc("Ribbon button clicked.");
-            openDialogAsIframe();
-            event.completed();
-        }
-
-function addTextToBody(text, icon, event) {
-    Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Text },
-        function (asyncResult) {
-            if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-                statusUpdate(icon, "\"" + text + "\" inserted successfully.");
-            }
-            else {
-                Office.context.mailbox.item.notificationMessages.addAsync("addTextError", {
-                    type: "errorMessage",
-                    message: "Failed to insert \"" + text + "\": " + asyncResult.error.message
-                });
-            }
-            event.completed();
-        });
-}
-
-
-
-function UpdateSubject(event)
-{
-    var body;
-    var Recipients;
-    Office.context.mailbox.item.cc.getAsync(callback);   
-    item.body.getAsync(
-        "html",
-        { asyncContext: "This is passed to the callback" },
-        function callback(result) {
-            body = result.value;
-
-            //Setting update body
-            Office.context.mailbox.item.body.setAsync(
-                "<b>" + body + "</b>",
-                { coercionType: "html", asyncContext: "This is passed to the callback" },
-                function callback(result) {
-                    if (result.status == Office.AsyncResultStatus.Succeeded)
-                    {
-                       
-                    }
-                });
-
-
-        });  
-    event.completed();
-}
-
-function PostDataToAPI(event)
-{
-    var item = Office.context.mailbox.item;
-    var uri = 'http://localhost:1300/api/cities';
-    var imapSetting = {
-        Subject: item.subject,
-        Body: 'helllos'       
-    }
-    $.ajax({
-        url: uri,
-        type: 'POST',
-        data: JSON.stringify(imapSetting),
-        contentType: "application/json",
-        success: function (d) {
-            //  alert("Saved Successfully");
-            //document.getElementById("postform").reset();
-            if (d = true) {
-                writemessage(d);
-            }
-        },
-        error: function () {
-            writemessage("errrpr");
-        }
-    });
-
-    event.completed();
-}
-
-function writemessage(messages)
-{
-   
-}
-
-function DisaplyBody(asyncResult) {
-
-    var currentbody = asyncResult.value;
-    addTextToBody(currentbody + " update Text", "red-icon-16", event);
-}
-function addMsg1ToBody(event) {
-    addTextToBody("Hello World!", "red-icon-16", event);
 }
